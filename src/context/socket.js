aa2943820989c26b305323dcf4d5eba6bf893922
Randre42 <113473773+Randre42@users.noreply.github.com>
2022-11-30 17:18:16 -0500

    Delete src directory

--- src/context/socket.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import socketio from 'socket.io-client';
-//import {SOCKET_URL} from './config';
-
-//so the user has the same socket connection when they go to different pages
-export const socket = socketio.connect('http://localhost:3001');
-export const SocketContext = React.createContext();
-
