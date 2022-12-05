4267a84964e82e01c99c7d4e2e3833696f32c498
Jeremy Mongeau <mongeaujeremy@gmail.com>
2022-12-05 17:45:09 -0500

    Changed Board to function

--- client/src/pages/Battleboard.js
+++ client/src/pages/Battleboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/battleboard.css';
 
@@ -10,166 +10,163 @@ import './css/battleboard.css';
       );
     }
   
-  class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(64).fill(null),
+  function Board() { 
+      const [state,setState] = useState({
+            squares: fillPieces(Array(64).fill(null)),
             redTurn: true,
             turnCount: 0,
             pieceClicked: -1, // This stores the index of the piece clicked. -1 means that we are still deciding on a piece to click
-        };
-        this.fillPieces();
+        });
+
+        return (
+          <div className="board-container">
+            <div className="status">{determineStatus(state)}</div>
+            {makeBoard(state, setState)}
+          </div>
+        );
     }
 
-    fillPieces() { // This will fill the Array, squares, with pieces. These pieces are Objects that tell which color the piece is, and whether it is a king
-      this.state.squares = this.state.squares.map((square, i) => {
-        if (i < 24 && this.colorAtIndex(i) == 'blackSquare') { // Places black pieces
-          return {
-            color: 'Black',
-            king: false,
-          };
-        }
-        else if (i > 39 && this.colorAtIndex(i) == 'blackSquare') { // Places red pieces
-          return {
-            color: 'Red',
-            king: false,
-          };
-        }
-      })
+  function makeBoard(state, setState) {
+    const arraySquares = state.squares.map(
+      (square, i) => <Square
+        value={square}
+        onClick={() => handleClick(i, state, setState)}
+        className = {state.pieceClicked == i ? 'selectedSquare' : colorAtIndex(i)}
+      />
+    )
+
+    const boardRows = []
+    for (let i=0;i<8;i++) { // This loop splits the board into rows, forming a chessboard
+      boardRows.push(( <div className="board-row">
+        {arraySquares.slice(i*8,i*8+8)}
+        </div>))
     }
 
-    validMoves(i) { /* Checks for valid moves and pushes the indices into an array, which it returns.
-                       i-9 is top left    | i-7 is top right
-                       i+7 is bottom left | i+9 is bottom right */
-        let result = []; // Result will be an array of Objects containing openSpace and jumpedPiece
-                         // openSpace will be the space a piece can move to as a valid move
-                         // jumpedPiece will be the index of a piece that will be jumped for a valid move
+    return boardRows;
+  }
 
-        if (i%8 != 0) { // Jumps for everything NOT in the first column
-          if (this.state.squares[i].color == 'Red' || this.state.squares[i].king) {
-            if (i > 9 && !this.state.squares[i-9]) result.push({openSpace: i-9, jumpedPiece: null})
-            if (i > 18 && !this.state.squares[i-18] && this.state.squares[i-9] 
-               && this.state.squares[i-9].color != this.state.squares[i].color) result.push({openSpace: i-18, jumpedPiece: i-9})
-          }
-          if (this.state.squares[i].color == 'Black' || this.state.squares[i].king) {
-            if (i < 56 && !this.state.squares[i+7]) result.push({openSpace: i+7, jumpedPiece: null})
-            if (i < 47 && !this.state.squares[i+14] && this.state.squares[i+7]
-              && this.state.squares[i+7].color != this.state.squares[i].color) result.push({openSpace: i+14, jumpedPiece: i+7})
-          }
-        }
-
-        if (i%8 != 7) { // Jumps for everything NOT in the last column
-          if (this.state.squares[i].color == 'Red' || this.state.squares[i].king) {
-            if (i > 7 && !this.state.squares[i-7]) result.push({openSpace: i-7, jumpedPiece: null})
-            if (i > 16 && !this.state.squares[i-14] && this.state.squares[i-7]
-              && this.state.squares[i-7].color != this.state.squares[i].color) result.push({openSpace: i-14, jumpedPiece: i-7})
-          }
-          if (this.state.squares[i].color == 'Black' || this.state.squares[i].king) {
-            if (i < 55 && !this.state.squares[i+9]) result.push({openSpace: i+9, jumpedPiece: null})
-            if (i < 46 && !this.state.squares[i+18] && this.state.squares[i+9]
-              && this.state.squares[i+9].color != this.state.squares[i].color) result.push({openSpace: i+18, jumpedPiece: i+9})
-          }
-        }
-
-        console.log(result);
-        return result;
-    }
-
-    handleClick(i) {    
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares)) {
-            return;
-        }
-        if (this.state.pieceClicked == -1 && this.state.redTurn && squares[i]?.color == 'Red') { // This checks if it is red's turn, and if piece clicked is red
-          if (this.validMoves(i).length == 0) return // If there is no valid moves, go back to piece selection without changing turns
-          this.setState({
-            squares: squares,
-            redTurn: this.state.redTurn,
-            pieceClicked: i, // Updates the current state to contain the index of piece clicked (if there are valid moves)
-          }, () => console.log(this.state.pieceClicked))
-          return;
-        }
-        if (this.state.pieceClicked == -1 && !this.state.redTurn && squares[i]?.color == 'Black') { // This checks if it is black's turn, and if piece clicked is black
-          if (this.validMoves(i).length == 0) return; // If there is no valid moves, go back to piece selection without changing turns
-          this.setState({
-            squares: squares,
-            redTurn: this.state.redTurn,
-            pieceClicked: i, // Updates the current state to contain the index of piece clicked (if there are valid moves)
-          }, () => console.log(this.state.pieceClicked))
-          return;
-        }
-        if (this.state.pieceClicked != -1) {
-          const validMove = this.validMoves(this.state.pieceClicked).find((validMove) => validMove.openSpace == i)
-          if (validMove) {
-            if (squares[this.state.pieceClicked].color == 'Red' && i < 8 || squares[this.state.pieceClicked].color == 'Black' && i > 55) {
-              squares[this.state.pieceClicked].king = true;
-            }
-            squares[i] = squares[this.state.pieceClicked];
-            squares[this.state.pieceClicked] = null;
-            if (validMove.jumpedPiece) squares[validMove.jumpedPiece] = null;
-            this.setState({
-              squares: squares,
-              redTurn: !this.state.redTurn,
-              pieceClicked: -1,
-            })
-          }
-            else this.setState({
-              squares:squares,
-              redTurn: this.state.redTurn,
-              pieceClicked: -1,
-            })
-        }
-
-        return;
-    }
-
-    colorAtIndex(i) { // This function dictates if a square on the board is to be colored white or black
-      if (i%16 < 8) {
-        return i%2 == 0 ? 'whiteSquare' : 'blackSquare'
-      }
-      else {
-        return i%2 == 0 ? 'blackSquare' : 'whiteSquare'
-      }
-    }
-    
-    render(square) {
-      const winner = calculateWinner(this.state.squares);
+  function determineStatus(state) {
+    const winner = calculateWinner(state.squares);
       let status;
       if (winner) {
         status = 'Winner: ' + winner;
       } else {
-        status = 'Next player: ' + (this.state.redTurn ? 'Red' : 'Black');
+        status = 'Next player: ' + (state.redTurn ? 'Red' : 'Black');
       }
-  
-      const arraySquares = this.state.squares.map(
-        (square, i) => <Square
-          value={square}
-          onClick={() => this.handleClick(i)}
-          className = {this.state.pieceClicked == i ? 'selectedSquare' : this.colorAtIndex(i)}
-        />
-      )
 
-      const boardRows = []
-      for (let i=0;i<8;i++) { // This loop splits the board into rows, forming a chessboard
-        boardRows.push(( <div className="board-row">
-          {arraySquares.slice(i*8,i*8+8)}
-          </div>))
-      }
-      return (
-        <div className="board-container">
-          <div className="status">{status}</div>
-          {boardRows}
-        </div>
-      );
-    }
+      return status;
   }
   
-  class Game extends React.Component {
-    render() {
-      return (
-            <Board />
-      );
+  function fillPieces(squares) { // This will fill the Array, squares, with pieces. These pieces are Objects that tell which color the piece is, and whether it is a king
+    return squares.map((square, i) => {
+      if (i < 24 && colorAtIndex(i) == 'blackSquare') { // Places black pieces
+        return {
+          color: 'Black',
+          king: false,
+        };
+      }
+      else if (i > 39 && colorAtIndex(i) == 'blackSquare') { // Places red pieces
+        return {
+          color: 'Red',
+          king: false,
+        };
+      }
+    })
+  }
+
+  function validMoves(i, squares) { /* Checks for valid moves and pushes the indices into an array, which it returns.
+                     i-9 is top left    | i-7 is top right
+                     i+7 is bottom left | i+9 is bottom right */
+      let result = []; // Result will be an array of Objects containing openSpace and jumpedPiece
+                       // openSpace will be the space a piece can move to as a valid move
+                       // jumpedPiece will be the index of a piece that will be jumped for a valid move
+
+      if (i%8 != 0) { // Jumps for everything NOT in the first column
+        if (squares[i].color == 'Red' || squares[i].king) {
+          if (i > 9 && !squares[i-9]) result.push({openSpace: i-9, jumpedPiece: null})
+          if (i > 18 && !squares[i-18] && squares[i-9] 
+             && squares[i-9].color != squares[i].color) result.push({openSpace: i-18, jumpedPiece: i-9})
+        }
+        if (squares[i].color == 'Black' || squares[i].king) {
+          if (i < 56 && !squares[i+7]) result.push({openSpace: i+7, jumpedPiece: null})
+          if (i < 47 && !squares[i+14] && squares[i+7]
+            && squares[i+7].color != squares[i].color) result.push({openSpace: i+14, jumpedPiece: i+7})
+        }
+      }
+
+      if (i%8 != 7) { // Jumps for everything NOT in the last column
+        if (squares[i].color == 'Red' || squares[i].king) {
+          if (i > 7 && !squares[i-7]) result.push({openSpace: i-7, jumpedPiece: null})
+          if (i > 16 && !squares[i-14] && squares[i-7]
+            && squares[i-7].color != squares[i].color) result.push({openSpace: i-14, jumpedPiece: i-7})
+        }
+        if (squares[i].color == 'Black' || squares[i].king) {
+          if (i < 55 && !squares[i+9]) result.push({openSpace: i+9, jumpedPiece: null})
+          if (i < 46 && !squares[i+18] && squares[i+9]
+            && squares[i+9].color != squares[i].color) result.push({openSpace: i+18, jumpedPiece: i+9})
+        }
+      }
+
+      return result;
+  }
+
+  function handleClick(i, state, setState) {    
+      if (calculateWinner(state.squares)) {
+          return;
+      }
+      if (state.pieceClicked == -1 && state.redTurn && state.squares[i]?.color == 'Red') { // This checks if it is red's turn, and if piece clicked is red
+        if (validMoves(i, state.squares).length == 0) return // If there is no valid moves, go back to piece selection without changing turns
+        setState({
+          squares: state.squares,
+          redTurn: state.redTurn,
+          turnCount: state.turnCount,
+          pieceClicked: i, // Updates the current state to contain the index of piece clicked (if there are valid moves)
+        })
+        return;
+      }
+      if (state.pieceClicked == -1 && !state.redTurn && state.squares[i]?.color == 'Black') { // This checks if it is black's turn, and if piece clicked is black
+        if (validMoves(i, state.squares).length == 0) return; // If there is no valid moves, go back to piece selection without changing turns
+        setState({
+          squares: state.squares,
+          redTurn: state.redTurn,
+          turnCount: state.turnCount,
+          pieceClicked: i, // Updates the current state to contain the index of piece clicked (if there are valid moves)
+        })
+        return;
+      }
+      if (state.pieceClicked != -1) {
+        const validMove = validMoves(state.pieceClicked, state.squares).find((validMove) => validMove.openSpace == i)
+        if (validMove) {
+          if (state.squares[state.pieceClicked].color == 'Red' && i < 8 || state.squares[state.pieceClicked].color == 'Black' && i > 55) {
+            state.squares[state.pieceClicked].king = true;
+          }
+          state.squares[i] = state.squares[state.pieceClicked];
+          state.squares[state.pieceClicked] = null;
+          if (validMove.jumpedPiece) state.squares[validMove.jumpedPiece] = null;
+          setState({
+            squares: state.squares,
+            redTurn: !state.redTurn,
+            turnCount: ++state.turnCount,
+            pieceClicked: -1,
+          })
+        }
+          else setState({
+            squares: state.squares,
+            redTurn: state.redTurn,
+            turnCount: state.turnCount,
+            pieceClicked: -1,
+          })
+      }
+
+      return;
+  }
+
+  function colorAtIndex(i) { // This function dictates if a square on the board is to be colored white or black
+    if (i%16 < 8) {
+      return i%2 == 0 ? 'whiteSquare' : 'blackSquare'
+    }
+    else {
+      return i%2 == 0 ? 'blackSquare' : 'whiteSquare'
     }
   }
   
@@ -182,6 +179,6 @@ import './css/battleboard.css';
   // ========================================
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<Game />);
+  root.render(<Board />);
 
   export default Board;
