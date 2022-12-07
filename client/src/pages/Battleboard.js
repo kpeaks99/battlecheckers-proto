4222fc6d44bf14628bf946bdfc060e62e349ed28
Randre42 <113473773+Randre42@users.noreply.github.com>
2022-12-06 20:05:30 -0500

    Add files via upload
    Merged a functional checkerboard

--- client/src/pages/Battleboard.js
+++ client/src/pages/Battleboard.js
@@ -1,81 +1,240 @@
-import React, { useEffect,useContext,useState, useRef } from "react"
+import React, { useEffect,useContext,useState,useRef } from "react"
 import { useLocation } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
 //link stylesheet style.css
-import './css/checker-style.css'; 
+//import './css/checker-style.css'; 
+import './css/battleboard.css';
 import io from 'socket.io-client';
+//used for global socket
 import { SocketContext } from "../context/socket";
 
-function Battleboard() {
 
-  //this is the same socket from the context
-  const socket = useContext(SocketContext);
-  
-//using react hook location to get info from home.js, for when game is created
-  const location = useLocation();
+function Square(props) {
+  return (
+    <button className={"square " + props.className} onClick={props.onClick}>
+        {props.value && <span className={`piece${props.value.color}`}>{props.value.king ? '♕' : 'O'}</span>}
+    </button> // The above statement will place a piece based on color and king state
+  );
+}
 
-  //info passed from home.js
-  //remember, false is RED, true is BLACK
-  //var playerColor = location.state.playerColor 
-  const gameID = location.state.gameID
-  //const socket = location.state.socket
+function Board() { 
 
-  //var squareNum = 1;
-  const [squareNum, setSquareNum] = useState("");
+  const location = useLocation(); //get information from MatchMaking.js
+  const socket = useContext(SocketContext); //get socket from context/socket.js
+  const gameID = location.state.gameID; //get gameID from MatchMaking.js
+
   const [playerColor, setPlayerColor] = useState(location.state.playerColor);
-  //setPlayerTurn(location.state.PlayerColor) 
-
-  //for testing if both players can contribute counting the squares
-  const clickCounter = () => {
-      console.log("square clicked " + playerColor);
-      if(playerColor){
-          //playerColor = false;
-          setPlayerColor(false);
-          socket.emit('square_click', gameID, location.state.playerColor);
-      }
-      //socket.emit('square_click', gameID);
-  };
 
 
+  const [state,setState] = useState({
+        squares: fillPieces(Array(64).fill(null)),
+        redTurn: true,
+        turnCount: 0,
+        pieceClicked: -1, // This stores the index of the piece clicked. -1 means that we are still deciding on a piece to click
+    });
 
   useEffect(() => {
-      //recieve new +1 data from a player clicking a square
-      socket.on('update_square', (data, setInteractive) => {
-          setSquareNum(data);
-          //playerColor = setInteractive;
-          setPlayerColor(setInteractive);
-          console.log("squareNum: " + data);
-          console.log("This is set to " + setInteractive);
 
+      //testing purposes
+      socket.on('test_recieve', (data) => {
+              console.log("test connection recieved!");
+              console.log(data);
+              //setResponse(data + " recieved!!");
+            });
+
+      //recieve board data from server
+      socket.on('update_board', (boardData, changeTurn) => {
+        console.log("board recieved!");
+        //setState(boardData);
+        setState({
+          squares: boardData.squares,
+          redTurn: !changeTurn,
+          turnCount: boardData.turnCount,
+          pieceClicked: -1,
+        });
+        console.log(state);
       });
 
   } ,[socket]);
+    //testing purposes
+    function TestConnection(){
+      //console.log("test connection");
+      socket.emit('test_connection', gameID);
+    }
 
-  return (
-      //display text in h2
-      <body>
-          <main>
-      <div>
-          <h2>Welcome to the game!</h2>   
-          
-          <h2>Player Turn: {playerColor ? "It's your turn!" : "Wait for turn..."}</h2>
-          
-          {/* if THIS playerColor is False, color it red */}
-          {location.state.playerColor ? <h2>You are red</h2> : <h2>You are black</h2>}
-          <h2>Game ID: {gameID}</h2>  
-
-          {/* for testing */}
-          {/* toggcle classname if playerColor is false or true */}
-          {/* <h2 className={playerColor ? "black-squareG" : "square"}>Player Turn: {playerColor ? "Black" : "Red"}</h2> */}
-           <div className={playerColor ? "squareG" : "square"} id="sq" onClick={clickCounter}> Test click counter for both sides {squareNum}</div>
-
-      </div>
-      </main>
-      </body>
-
+    //send board data to server
+    function SendGameStateToServer(){
+      socket.emit('board_update', state, gameID, location.state.playerColor);
       
+    }
 
-  );
+    function makeBoard(state, setState) {
+      const arraySquares = state.squares.map(
+        (square, i) => <Square
+          value={square}
+          onClick={() => HandleClick(i, state, setState)}
+          className = {state.pieceClicked == i ? 'selectedSquare' : colorAtIndex(i)}
+        />
+      )
+      
+      const boardRows = []
+      for (let i=0;i<8;i++) { // This loop splits the board into rows, forming a chessboard
+        boardRows.push(( <div className="board-row">
+          {arraySquares.slice(i*8,i*8+8)}
+          </div>))
+      }
 
+      return boardRows;
+  }
+
+  function determineStatus(state) {
+  const winner = calculateWinner(state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+      //may send data to database for win/loss
+
+    } else {
+      status = 'Next player: ' + (state.redTurn ? 'Red' : 'Black');
+    }
+  
+    return status;
+  }
+
+  function fillPieces(squares) { // This will fill the Array, squares, with pieces. These pieces are Objects that tell which color the piece is, and whether it is a king
+    return squares.map((square, i) => {
+      if (i < 24 && colorAtIndex(i) == 'blackSquare') { // Places black pieces
+        return {
+          color: 'Black',
+          king: false,
+        };
+      }
+      else if (i > 39 && colorAtIndex(i) == 'blackSquare') { // Places red pieces
+        return {
+          color: 'Red',
+          king: false,
+        };
+      }
+    })
+  }
+  function validMoves(i, squares) { /* Checks for valid moves and pushes the indices into an array, which it returns.
+                 i-9 is top left    | i-7 is top right
+                 i+7 is bottom left | i+9 is bottom right */
+  let result = []; // Result will be an array of Objects containing openSpace and jumpedPiece
+                   // openSpace will be the space a piece can move to as a valid move
+                   // jumpedPiece will be the index of a piece that will be jumped for a valid move
+
+  if (i%8 != 0) { // Jumps for everything NOT in the first column
+    if (squares[i].color == 'Red' || squares[i].king) {
+      if (i > 9 && !squares[i-9]) result.push({openSpace: i-9, jumpedPiece: null})
+      if (i > 18 && !squares[i-18] && squares[i-9] 
+         && squares[i-9].color != squares[i].color) result.push({openSpace: i-18, jumpedPiece: i-9})
+    }
+    if (squares[i].color == 'Black' || squares[i].king) {
+      if (i < 56 && !squares[i+7]) result.push({openSpace: i+7, jumpedPiece: null})
+      if (i < 47 && !squares[i+14] && squares[i+7]
+        && squares[i+7].color != squares[i].color) result.push({openSpace: i+14, jumpedPiece: i+7})
+    }
+  }
+
+  if (i%8 != 7) { // Jumps for everything NOT in the last column
+    if (squares[i].color == 'Red' || squares[i].king) {
+      if (i > 7 && !squares[i-7]) result.push({openSpace: i-7, jumpedPiece: null})
+      if (i > 16 && !squares[i-14] && squares[i-7]
+        && squares[i-7].color != squares[i].color) result.push({openSpace: i-14, jumpedPiece: i-7})
+    }
+    if (squares[i].color == 'Black' || squares[i].king) {
+      if (i < 55 && !squares[i+9]) result.push({openSpace: i+9, jumpedPiece: null})
+      if (i < 46 && !squares[i+18] && squares[i+9]
+        && squares[i+9].color != squares[i].color) result.push({openSpace: i+18, jumpedPiece: i+9})
+    }
+  }
+
+  return result;
+  }
+
+  function HandleClick(i, state, setState) {    
+  if (calculateWinner(state.squares)) {
+      return;
+  }                                                                                 // AND this client user is red
+  if (state.pieceClicked == -1 && state.redTurn && state.squares[i]?.color == 'Red' && location.state.playerColor) { // This checks if it is red's turn, and if piece clicked is red
+    if (validMoves(i, state.squares).length == 0) return // If there is no valid moves, go back to piece selection without changing turns
+    setState({
+      squares: state.squares,
+      redTurn: state.redTurn,
+      turnCount: state.turnCount,
+      pieceClicked: i, // Updates the current state to contain the index of piece clicked (if there are valid moves)
+    })
+    return;
+  }                                                                                   //  AND this client user is black
+  if (state.pieceClicked == -1 && !state.redTurn && state.squares[i]?.color == 'Black' && !location.state.playerColor) { // This checks if it is black's turn, and if piece clicked is black
+    if (validMoves(i, state.squares).length == 0) return; // If there is no valid moves, go back to piece selection without changing turns
+    setState({
+      squares: state.squares,
+      redTurn: state.redTurn,
+      turnCount: state.turnCount,
+      pieceClicked: i, // Updates the current state to contain the index of piece clicked (if there are valid moves)
+    })
+    return;
+  }
+  if (state.pieceClicked != -1) {
+    const validMove = validMoves(state.pieceClicked, state.squares).find((validMove) => validMove.openSpace == i)
+    if (validMove) {
+      if (state.squares[state.pieceClicked].color == 'Red' && i < 8 || state.squares[state.pieceClicked].color == 'Black' && i > 55) {
+        state.squares[state.pieceClicked].king = true;
+      }
+      state.squares[i] = state.squares[state.pieceClicked];
+      state.squares[state.pieceClicked] = null;
+      if (validMove.jumpedPiece) state.squares[validMove.jumpedPiece] = null;
+      setState({
+        squares: state.squares,
+        redTurn: !state.redTurn,
+        turnCount: ++state.turnCount,
+        pieceClicked: -1,
+      })
+      //send the board state and current playerColor to the server
+      //TestConnection();
+      SendGameStateToServer();
+    }
+      else 
+      {
+        setState({
+        squares: state.squares,
+        redTurn: state.redTurn,
+        turnCount: state.turnCount,
+        pieceClicked: -1,
+      })
+      SendGameStateToServer();
+      }
+      //TestConnection();
+      
+  }
+
+  return;
+  }
+
+  function colorAtIndex(i) { // This function dictates if a square on the board is to be colored white or black
+if (i%16 < 8) {
+  return i%2 == 0 ? 'whiteSquare' : 'blackSquare'
+}
+else {
+  return i%2 == 0 ? 'blackSquare' : 'whiteSquare'
+}
+  }
+
+  function calculateWinner(squares) {
+if (!squares.find((piece)=>piece?.color == 'Black')) return 'Red'; // Checks if there are any Black pieces remaining. If not, Red wins
+if (!squares.find((piece)=>piece?.color == 'Red')) return 'Black'; // Checks if there are any Red pieces remaining. If not, Black wins
+return null;
+  }
+
+    return (
+      <div className="board-container">
+        {location.state.playerColor ? <h2 >You are RED</h2> : <h2>You are BLACK</h2>} 
+        <div className="status">{determineStatus(state)}</div>
+        {makeBoard(state, setState)}
+      </div>
+    );
 }
 
-export default Battleboard
+export default Board;
