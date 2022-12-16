6c6a07c90637580b1639476e89793c2807cbe05c
Avery Romano <AveryR9598@yahoo.com>
2022-12-16 01:42:56 -0500

    fixed small error

--- server/middleware/Auth.js
+++ server/middleware/Auth.js
@@ -1,5 +1,3 @@
-
-
 const {verify} = require('jsonwebtoken');
 
 const validateToken = (req,res,next) => {
--- server/routes/loginreg.js
+++ server/routes/loginreg.js
@@ -45,7 +45,6 @@ router.post("/login", async (req,res) => {
             );
 
         res.json({token: webToken, username: user.username, id: user.id});
-        res.json({token: webToken, username: user.name, id: user.id})
         console.log(webToken)
         console.log(user.id)
     })
